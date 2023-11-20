59fdd99f67b7450b77227e6c80d65e0d31af5acc
babyo77 <144552425+babyo77@users.noreply.github.com>
2023-11-20 10:12:58 +0530

    Update video.js

--- public/video.js
+++ public/video.js
@@ -32,8 +32,8 @@ navigator.mediaDevices.getUserMedia({
     });
     socket.on('peer',()=>{
         peer = new Peer(undefined, {
-            host: "https://wicked-gray-sun-hat.cyclic.app",
-            port: 3000,
+            host: "wicked-gray-sun-hat.cyclic.app",
+            port: 443,
             path: "/",
             config: {
                 iceServers: [
